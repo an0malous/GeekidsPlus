093d61ae0a37ee9742977dc803c00beb0aa840e1
an0malous <anomalous58@gmail.com>
2020-10-10 23:15:35 +0900

    added blends section in CardForm

--- frontend/src/pages/admin-dashboard/cards-section/CardForm.js
+++ frontend/src/pages/admin-dashboard/cards-section/CardForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 export function CardForm({
   handleChange,
@@ -10,68 +10,88 @@ export function CardForm({
   img,
   audio,
 }) {
+  {
+    console.log(type);
+  }
   return (
-      <form className="ui form" onSubmit={onSubmit}>
+    <form className="ui form" onSubmit={onSubmit}>
+      <div className="field">
+        <label>Word Label</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="name"
+          value={name}
+          placeholder="word name"
+        />
+      </div>
+      <div className="two fields">
         <div className="field">
-          <label>Word Label</label>
-          <input
+          <label>Type</label>
+          <select
             onChange={handleChange}
-            type="text"
-            name="name"
-            value={name}
-            placeholder="word name"
-          />
-        </div>
-        <div className="two fields">
-          <div className="field">
-            <label>Type</label>
-            <select
-              onChange={handleChange}
-              name="type"
-              value={type}
-              className="ui fluid dropdown"
-            >
-              <option value="" disabledSelect>
-                Choose a Type
-              </option>
-              <option value="cvc">CVC</option>
-              <option value="cvcAdd">CVC Add on</option>
-              <option value="blends">Blends</option>
-            </select>
-          </div>
-          <div className="field">
-            <label>Letter</label>
-            <select
-              onChange={handleChange}
-              name="letter"
-              className="ui fluid dropdown"
-              value={letter}
-            >
-              <option value="" disabledSelect>
-                Choose a Letter
-              </option>
-              <option value="a">a</option>
-              <option value="e">e</option>
-              <option value="i">i</option>
-              <option value="o">o</option>
-              <option value="u">u</option>
-              <option value="y">y</option>
-            </select>
-          </div>
+            name="type"
+            value={type}
+            className="ui fluid dropdown"
+          >
+            <option value="" disabledSelect>
+              Choose a Type
+            </option>
+            <option value="cvc">CVC</option>
+            <option value="cvcAdd">CVC Add on</option>
+            <option value="blends">Blends</option>
+          </select>
         </div>
         <div className="field">
-          <label>Image url</label>
-          <input
+          <label>Letter</label>
+          <select
+            id="card-form-select"
             onChange={handleChange}
-            type="text"
-            name="img"
-            value={img}
-            placeholder="image url"
-          />
+            name="letter"
+            className="ui fluid dropdown"
+            value={letter}
+          >
+            {type !== "blends" ? (
+              <Fragment>
+                <option value="" disabledSelect>
+                  Choose a Letter
+                </option>
+                <option value="a">a</option>
+                <option value="e">e</option>
+                <option value="i">i</option>
+                <option value="o">o</option>
+                <option value="u">u</option>
+                <option value="y">y</option>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <option value="" disabledSelect>
+                  Choose a Letter
+                </option>
+                <option value="ch">ch</option>
+                <option value="sh">sh</option>
+                <option value="wh">wh</option>
+                <option value="th">th</option>
+                <option value="ck">ck</option>
+                <option value="ng">ng</option>
+              </Fragment>
+            )}
+          </select>
         </div>
-        <button className="ui button" type="submit">
-          {btnText}
-        </button>
-      </form>
+      </div>
+      <div className="field">
+        <label>Image url</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="img"
+          value={img}
+          placeholder="image url"
+        />
+      </div>
+      <button className="ui button" type="submit">
+        {btnText}
+      </button>
+    </form>
   );
 }
