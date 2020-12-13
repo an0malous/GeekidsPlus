c1fe308ec4d0f3fb6decadd0bd9dcba1d39c78ee
an0malous <anomalous58@gmail.com>
2020-12-14 00:17:38 +0900

    cleanup & refactor

--- frontend/src/components/admin-dashboard/tab.js
+++ frontend/src/components/admin-dashboard/tab.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { NavLink, useRouteMatch } from 'react-router-dom';
 import { withRouter } from 'react-router'
 
-const Tab = ({match, location: { pathname }, items}) => {
-    const { url, path } = useRouteMatch();
-    console.log(match)
+const Tab = ({items}) => {
+    const { url } = useRouteMatch();
+ 
     return (
              items.map((item)=>{
                  return (
