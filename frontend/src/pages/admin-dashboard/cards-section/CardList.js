7a17e6e33e6fbc64c196868c9b3a0f01304c935e
an0malous <anomalous58@gmail.com>
2020-12-14 00:18:13 +0900

    fixed html table errors showing up in the console

--- frontend/src/pages/admin-dashboard/cards-section/CardList.js
+++ frontend/src/pages/admin-dashboard/cards-section/CardList.js
@@ -2,59 +2,64 @@ import React from 'react';
 import api from '../../../api';
 import { DynamicListItem as ListItem } from '../components/DynamicListItem';
 
-export  default class CardList extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {cards:[]}
-        this.deleteItem = this.deleteItem.bind(this)
-    }
+export default class CardList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { cards: [] };
+		this.deleteItem = this.deleteItem.bind(this);
+	}
 
-    getCards = async () => {
-        try{
-            const res = await api.getCards();
-            this.setState({cards: res.data})
-        } catch (err){
-            console.log(err);
-        } 
-    }
+	getCards = async () => {
+		try {
+			const res = await api.getCards();
+			this.setState({ cards: res.data });
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-    componentDidMount(){
-        this.getCards();
-    }
+	componentDidMount() {
+		this.getCards();
+	}
 
-    deleteItem = async id => {
-        if(id){
-            try{
-                const res = await api.deleteCard(id)
-                console.log(res.data);  
-                this.getCards();
-            } catch(err){
-                console.log(`Sorry, the request could not be made. Error: ${err}`);
-            }
-        }
-    }
+	deleteItem = async (id) => {
+		if (id) {
+			try {
+				const res = await api.deleteCard(id);
+				console.log(res.data);
+				this.getCards();
+			} catch (err) {
+				console.log(
+					`Sorry, the request could not be made. Error: ${err}`
+				);
+			}
+		}
+	};
 
-    render(){
-
-        return (
-            <div>
-                <div className="ui relaxed divided list">
-                    <table class="ui striped table">
-                        <thead>
-                            <th>Name</th>
-                            <th>Created</th>
-                            <th>Last Updated</th>
-                            <th>Actions</th>
-                        </thead>
-                        {this.state.cards !== [] ? this.state.cards.map((card)=>(
-                            <ListItem mappedItem={card} deleteItem={this.deleteItem} />
-                    )) : null}
-                    </table>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<div className="ui relaxed divided list">
+					<table class="ui striped table">
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Created</th>
+								<th>Last Updated</th>
+								<th>Actions</th>
+							</tr>
+						</thead>
+						{this.state.cards !== []
+							? this.state.cards.map((card) => (
+									<ListItem
+										mappedItem={card}
+										deleteItem={this.deleteItem}
+									/>
+							  ))
+							: null}
+					</table>
+				</div>
+			</div>
+		);
+	}
 }
-
-
-   
--- frontend/src/pages/admin-dashboard/components/DynamicListItem.js
+++ frontend/src/pages/admin-dashboard/components/DynamicListItem.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import { Link, useRouteMatch } from 'react-router-dom'
+import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 export const DynamicListItem = ({ mappedItem, deleteItem }) => {
-    const { url } = useRouteMatch();
-    return (
-        <tr>
-            <td><Link to={`${url}/edit/${mappedItem._id}`}>{mappedItem.name}</Link></td>
-            <td>by {mappedItem.author.username} at {new Date(mappedItem.createdAt).toLocaleDateString()} </td>
-            <td>by {mappedItem.author.username} at {new Date(mappedItem.updatedAt).toLocaleDateString()}</td>
-            <td>
-                <a onClick={()=>{deleteItem(mappedItem._id)}}>Delete</a>
-            </td>
-        </tr>
-    )
-}
+	const { url } = useRouteMatch();
+	return (
+		<tbody>
+			<tr>
+				<td>
+					<Link to={`${url}/edit/${mappedItem._id}`}>
+						{mappedItem.name}
+					</Link>
+				</td>
+				<td>
+					by {mappedItem.author.username} at{' '}
+					{new Date(mappedItem.createdAt).toLocaleDateString()}{' '}
+				</td>
+				<td>
+					by {mappedItem.author.username} at{' '}
+					{new Date(mappedItem.updatedAt).toLocaleDateString()}
+				</td>
+				<td>
+					<a
+						onClick={() => {
+							deleteItem(mappedItem._id);
+						}}
+					>
+						Delete
+					</a>
+				</td>
+			</tr>
+		</tbody>
+	);
+};
