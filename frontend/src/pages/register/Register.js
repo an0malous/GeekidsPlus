82cdb7645fdcf0c45707bb91a828f8981258fc1c
an0malous <anomalous58@gmail.com>
2020-12-17 02:24:06 +0900

    changed register page over to hooks

--- frontend/src/pages/register/Register.js
+++ frontend/src/pages/register/Register.js
@@ -1,78 +1,75 @@
-import React from "react";
-import api from "../../api";
+import React from 'react';
+import { useHistory } from 'react-router';
+import api from '../../api';
+import useInput from '../../custom-hooks/use-Input';
 
-export default class Register extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: "", password: "" };
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onChange = this.onChange.bind(this);
-  }
+const Register = () => {
+	let history = useHistory();
+	const {inputs, handleInputChange, handleSubmit } = useInput(
+		async function () {
+			if (inputs.password && inputs.username) {
+				try {
+					const { username, password } = inputs;
+					const payload = { username, password };
+					const response = await api.register(payload);
 
-    onChange (event){
-      this.setState({[event.target.name]: event.target.value})
-    }
+					if (!response.data.errmsg) {
+						console.log('successful signup');
+						history.push("/");
+					} else {
+						console.log('username already taken');
+					}
+				} catch (error) {
+					console.log('signup error: ');
+					console.log(error);
+				}
+			}
+		}
+	);
+	console.log(history)
+	return (
+		<div>
+			<form className="ui form">
+				<div className="field">
+					<label>Username</label>
+					<input
+						onChange={handleInputChange}
+						value={inputs.username}
+						type="text"
+						name="username"
+						placeholder="username"
+					/>
+				</div>
+				<div className="field">
+					<label>Password</label>
+					<input
+						onChange={handleInputChange}
+						value={inputs.password}
+						type="password"
+						name="password"
+						placeholder="password"
+					/>
+				</div>
+				<div className="field">
+					<label>Confirm password</label>
+					<input
+						onChange={handleInputChange}
+						value={inputs.confirmPassword}
+						type="password"
+						name="last-name"
+						placeholder="confirmation password"
+					/>
+				</div>
+				<button
+					className="ui button"
+					type="submit"
+					onClick={handleSubmit}
+				>
+					Sign up
+				</button>
+			</form>
+		</div>
+	);
+};
 
-    onSubmit = async event => {
-      console.log("sign-up handleSubmit, username: ");
-      console.log(this.state.username);
-      event.preventDefault();
-  
-      const {username, password} = this.state;
-      if(password && username){
-        try{
-          const payload = {username, password}
-          const response = await api.register(payload);
-          console.log(response);
-          if (!response.data.errmsg) {
-            console.log("successful signup");
-            window.location = "/login";
-          } else {
-            console.log("username already taken");
-          }
-        } catch(error) {
-          console.log("signup error: ");
-          console.log(error);
-        };
-      }
-    };  
-  
-  render() {
-    return (
-      <div>
-        <form className="ui form">
-          <div className="field">
-            <label>Username</label>
-            <input
-              onChange={this.onChange}
-              type="text"
-              name="username"
-              placeholder="username"
-            />
-          </div>
-          <div className="field">
-            <label>Password</label>
-            <input
-              onChange={this.onChange}
-              type="password"
-              name="password"
-              placeholder="password"
-            />
-          </div>
-          <div className="field">
-            <label>Confirm password</label>
-            <input
-              onChange={this.onChange}
-              type="password"
-              name="last-name"
-              placeholder="confirmation password"
-            />
-          </div>
-          <button className="ui button" type="submit" onClick={this.onSubmit}>
-            Sign up
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+export default Register;
