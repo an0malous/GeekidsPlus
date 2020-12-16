7f2f6c1b33ddebaf3c011346ae799c401352d115
an0malous <anomalous58@gmail.com>
2020-12-17 01:22:17 +0900

    changed login component to hooks

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -3,63 +3,55 @@ import api from '../../api';
 import { Redirect } from 'react-router-dom';
 import { getCurrentUser } from '../../actions/userActions';
 import { connect } from 'react-redux';
+import useInput from '../../custom-hooks/use-Input';
+import { useHistory } from 'react-router'
+const Login = ({ getCurrentUser }) => { 
+    let history = useHistory();
 
-class Login extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {username: '', password: '', redirectTo: null};
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChange = this.onChange.bind(this);
-   
-    }
-  
-    onSubmit = async event =>{
-        event.preventDefault()
-        const { username, password } = this.state;
-        if(username && password) {
+    const { inputs, handleInputChange, handleSubmit } = useInput(
+        async event =>{
+          
             
-            try {
-                const payload = {username: this.state.username, password: this.state.password} ;
-                const response = await api.login(payload);
-                const { username, role } = response.data;
-                console.log(response)
-                  if(username, role){
-                    this.props.getCurrentUser({ loggedIn: true, username: username, role: role })
-                    this.setState({redirectTo: "/"})
-                } else {
-            console.log("No username or password")
-            }
-        }catch(err){
-            console.log(err)
-          }
-        } else {console.log("No username or password")}
-        
-    };
+            if(inputs.username && inputs.password) {
+                
+                try {
+                    const payload = {username: inputs.username, password: inputs.password} ;
+                    const response = await api.login(payload);
+                    const { username, role } = response.data;
+                    
+                      if(username, role){
+                        getCurrentUser({ loggedIn: true, username, role })
+                        history.goBack();
+                    } else {
+                console.log("No username or password")
+                }
+            }catch(err){
+                console.log(err)
+              }
+            } else {console.log("No username or password")} 
+        }
+    );
     
-    onChange = (event) =>{
-        this.setState({[event.target.name]: event.target.value})
-    }
+    
 
-    render(){
         return (
-            this.state.redirectTo ? (<Redirect to="/" />) : (
+          
             <div className="ui container aligned center">
                 <form className="ui form">
                     <div className="field">
                         <label>Username</label>
-                        <input onChange={this.onChange} type="text" name="username" placeholder="username" />
+                        <input onChange={handleInputChange} type="text" name="username" value={inputs.username} placeholder="username" />
                     </div>
                     <div className="field">
                         <label>Password</label>
-                        <input onChange={this.onChange} type="password" name="password" placeholder="password" />
+                        <input onChange={handleInputChange} type="password" value={inputs.password} name="password" placeholder="password" />
                     </div>
-                    <button className="ui button" type="submit" onClick={this.onSubmit}>Login</button>
+                    <button className="ui button" type="submit" onClick={handleSubmit}>Login</button>
                </form>
             </div>
-            )
-        )
-    }
+            )   
 };
+
 
 const mapDispatchToProps = dispatch => ({
     getCurrentUser: (user) => dispatch(getCurrentUser(user))
