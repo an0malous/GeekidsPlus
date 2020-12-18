89e4ccc13ee7541769a69852ce09def2265494d4
an0malous <anomalous58@gmail.com>
2020-12-19 00:50:14 +0900

    styled login page with slogan and buttons

--- frontend/src/pages/landing/Landing.js
+++ frontend/src/pages/landing/Landing.js
@@ -1,23 +1,44 @@
 import React from 'react';
-
+import { Grid, Button } from 'semantic-ui-react';
+import { LandingContainer } from './landing.styles';
+import { useHistory } from 'react-router';
 export const Landing = (props) => {
-  
-    return (
-        <div>
-            <div style={{
-                position: "absolute",
-                maxWidth: "600px",
-                height: "auto",
-                left: "3rem",
-                lineHeight: "1",
-                top: "35%",
-                Zindex: -1,
-                fontSize: "3rem",
-                color: "white",
-                textShadow: "2px 2px black"}}>
-                Your Cirriculum. Anytime. Anywhere.
-            </div>
-        </div>
-
-    )
-}
+	let history = useHistory();
+	const handleButtonClick = (e) => {
+		history.push(`/${e.target.name}`);
+	};
+	return (
+		<LandingContainer>
+			<div>
+				<div
+					style={{
+						maxWidth: '600px',
+						lineHeight: '1',
+						paddingBottom: '10px',
+						fontSize: '3rem',
+						color: 'white',
+						textShadow: '2px 2px black',
+					}}
+				>
+					Your Cirriculum. Anytime. Anywhere.
+				</div>
+				<Button
+					primary
+					name="register"
+					onClick={handleButtonClick}
+					size="big"
+				>
+					Get Started
+				</Button>
+				<Button
+					name="login"
+					onClick={handleButtonClick}
+					secondary
+					size="big"
+				>
+					Login
+				</Button>
+			</div>
+		</LandingContainer>
+	);
+};
---
+++ frontend/src/pages/landing/landing.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const RegisterContainer = styled.div`
+background: rgba(33, 33, 33, 0.8);
+padding: 20px;
+
+`
+
+export const LandingContainer = styled.div`
+display: flex;
+margin-top: -60px;
+padding: 30px;
+align-items: center;
+height: 100vh;
+`
