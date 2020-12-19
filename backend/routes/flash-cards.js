ee7c9edec3b033241f40c4d16fe7f1ce09d4010f
an0malous <anomalous58@gmail.com>
2020-12-19 23:02:36 +0900

    added 'authenticated' middleware in to some routes and took it off the get route

--- backend/routes/flash-cards.js
+++ backend/routes/flash-cards.js
@@ -1,64 +1,63 @@
-const authenticated = require('../middleware/auth')
-const express = require('express')
-const router = express.Router()
-const FlashCard = require('../database/models/flash-card')
+const authenticated = require('../middleware/auth');
+const express = require('express');
+const router = express.Router();
+const FlashCard = require('../database/models/flash-card');
 
 //Add a card
-router.post("/add", (req, res)=>{
-    const { name, type, letter, img, audio } = req.body;
+router.post('/add', authenticated, (req, res) => {
+	const { name, type, letter, img, audio } = req.body;
 
-    const newCard = new FlashCard({
-        name,
-        type,
-        letter,
-        img,
-        audio,
-        decks: null,
-        author: {
-            id: req.user._id,
-            username: req.user.username
-        }
-    });
-    newCard.save()
-    .then(()=>res.json('New Phonics Card added:' + newCard))
-    .catch((err)=>res.json(`Sorry, ${err}`))
-})
+	const newCard = new FlashCard({
+		name,
+		type,
+		letter,
+		img,
+		audio,
+		decks: null,
+		author: {
+			id: req.user._id,
+			username: req.user.username,
+		},
+	});
+	newCard
+		.save()
+		.then(() => res.json('New Phonics Card added:' + newCard))
+		.catch((err) => res.json(`Sorry, ${err}`));
+});
 
 // Edit a card
-router.get("/edit/:id", (req, res)=>{
-    FlashCard.findById(req.params.id)
-    .then((card)=>res.json(card))
-})
+router.get('/edit/:id', (req, res) => {
+	FlashCard.findById(req.params.id).then((card) => res.json(card));
+});
 
 //Update a card
-router.post("/edit/:id", authenticated, (req, res)=>{
-FlashCard.findByIdAndUpdate(req.params.id)
-.then((card)=>{
-    const { name, type, letter, img, audio } = req.body
-    card.name = name;
-    card.type = type;
-    card.letter = letter;
-    card.img = img;
-    card.audio = audio;
+router.post('/edit/:id', authenticated, (req, res) => {
+	FlashCard.findByIdAndUpdate(req.params.id).then((card) => {
+		const { name, type, letter, img, audio } = req.body;
+		card.name = name;
+		card.type = type;
+		card.letter = letter;
+		card.img = img;
+		card.audio = audio;
 
-    card.save()
-    .then(()=>res.json('Card updated:' + card))
-    .catch((err)=>req.json(`Sorry, ${err}`))
-})
-})
+		card.save()
+			.then(() => res.json('Card updated:' + card))
+			.catch((err) => req.json(`Sorry, ${err}`));
+	});
+});
 
 //Get cards list
-router.get("/", authenticated, (req, res)=> {
-FlashCard.find()
-.then((cards)=> res.json(cards))
-.catch(err=> res.status(400).json(`Error: ${err}`))
-})
+router.get('/', (req, res) => {
+	FlashCard.find()
+		.then((cards) => res.json(cards))
+		.catch((err) => res.status(400).json(`Error: ${err}`));
+});
 
 //Delete a card
-router.delete("/:id", (req, res)=> {
-    FlashCard.findByIdAndDelete(req.params.id)
-    .then((card)=>res.json(card))
-    .catch(err=> res.status(400).json(`Error: ${err}`))
-})
+router.delete('/:id', (req, res) => {
+	FlashCard.findByIdAndDelete(req.params.id)
+		.then((card) => res.json(card))
+		.catch((err) => res.status(400).json(`Error: ${err}`));
+});
 
 module.exports = router;
