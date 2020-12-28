6817c8006bc803e97ba98adf5d0f206d4761a4bd
an0malous <anomalous58@gmail.com>
2020-12-28 23:43:31 +0900

    phonics game now opens up in a modal

--- frontend/src/pages/phonics-game-new/phonics-game.page.jsx
+++ frontend/src/pages/phonics-game-new/phonics-game.page.jsx
@@ -1,14 +1,42 @@
 import React, { useState } from 'react';
-
+import { Button, Header, Icon, Modal } from 'semantic-ui-react';
 import PhonicsGameContainer from './components/phonics-game.container';
+import { connect } from 'react-redux';
+import { onGameEnd } from '../../actions/phonicsGameActions';
 
-const PhonicsGamePage = () => {
-
-    return (
-        <div>
-            <PhonicsGameContainer />
-        </div>
-    )
+const PhonicsGamePage = ({ onGameEnd }) => {
+	const [open, setOpen] = useState(true);
+	return (
+		<Modal
+			basic
+			onClose={() => setOpen(false)}
+			onOpen={() => setOpen(true)}
+			open={open}
+			size="small"
+		>
+			<Header icon>Phonics</Header>
+			<Modal.Content>
+				<PhonicsGameContainer />
+			</Modal.Content>
+			<Modal.Actions>
+				<Button
+					basic
+					color="red"
+					inverted
+					onClick={() => {
+						setOpen(false);
+						onGameEnd();
+					}}
+				>
+					<Icon name="remove" /> Exit Phonics
+				</Button>
+			</Modal.Actions>
+		</Modal>
+	);
 };
 
-export default PhonicsGamePage;
+const mapDispatchToProps = (dispatch) => ({
+	onGameEnd: () => dispatch(onGameEnd()),
+});
+
+export default connect(null, mapDispatchToProps)(PhonicsGamePage);
